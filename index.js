dc47754882e46eafaa7f1b7f0ea6a0ecff79d3ce
Mubashir45191 <mubashirhasan716@gmail.com>
2020-08-17 20:29:54 +0530

    Vue is awesome

--- index.js
+++ index.js
@@ -7,5 +7,9 @@ var app = new Vue({
     image: "images/vuestocks.png",
     link:
       "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+    inventory:50,
+    onSale:true
   },
 });
+
+//a vue instance
