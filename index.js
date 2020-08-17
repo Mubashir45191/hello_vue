af92eaeaf9bf7955fee39a76683a2045ae9a880c
Mubashir45191 <mubashirhasan716@gmail.com>
2020-08-17 20:04:44 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    product: "Socks",
+    description:
+      " We offer awesome socks of amazing quality which you will surely love to wear and will become your favorite socks in  a moment of time.",
+    image: "images/vuestocks.png",
+    link:
+      "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+  },
+});
