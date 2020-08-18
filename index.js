e6a8ae13f421e76e00e0efd660a3e4259f3a95e0
Mubashir45191 <mubashirhasan716@gmail.com>
2020-08-18 17:46:27 +0530

    cool vue

--- index.js
+++ index.js
@@ -7,9 +7,21 @@ var app = new Vue({
     image: "images/vuestocks.png",
     link:
       "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-    inventory:50,
-    onSale:true,
-    details: ["80% cotton","20% polyster","Gender-neutral"]
+    inventory: 50,
+    onSale: true,
+    details: ["80% cotton", "20% polyster", "Gender-neutral"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "green",
+      },
+
+      {
+        variantId: 2235,
+        variantColor: "blue",
+      },
+    ],
+    sizes:["Small","Medium","Large","Extra-Large"]
   },
 });
 
